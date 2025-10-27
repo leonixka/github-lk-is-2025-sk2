0c50152f58959ae57621b861e420862defb13c58
leonixka <leona.krajzingerova@tul.cz>
2025-10-27 18:23:37 +0100

    #6 obdelnik

---
+++ IS Programy/program 004 - vykreslování obrazců #6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+//Šířka obdelníku
+int width;
+
+//Výška obdelníku
+int height;
+
+Console.Clear();
+
+Console.WriteLine("Zadejte šířku obdelníku:");
+width = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Zadejte výšku obdelníku:");
+height = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < height; i++)   {
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
---
+++ IS Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Runtime.InteropServices.Marshalling;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***********Nejvetsi ze tri cisel*************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Leona Krajzingerová *********");
+    Console.WriteLine("************** 23.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+    Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+    }
+    
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B: ");
+    }
+
+     Console.Write("Zadejte celé číslo C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+    Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu C: ");
+    }
+Console.WriteLine();
+    //chceme seřadit čísla  vzestupně
+    
+    
+    
+    }
